73402b4482e68efc108be7275f722f3bef7d96b9
peterpango <101198369+peterpango@users.noreply.github.com>
2022-07-08 23:26:06 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -41,11 +41,12 @@ def ans(name):
     req = urllib.request.Request(url1+'.html')
     response = urllib.request.urlopen(req)
     the_page = response.read().decode('utf-8')
-
+    s=''
     paper_title = re.findall('<span class="title" itemprop="name">(.*?)</span>',the_page,re.S)#检索页面中给的论文名字
+    print(paper_title[0])
     paper_web = re.findall('view</b></p><ul><li class="ee"><a href="(.*?)" itemprop="url">',the_page,re.S)#存储对应网址
+    print(paper_web[0]) 
     for i in range(len(paper_web)):
-        if (paper_web[i].find("arxiv") != -1): #查看网址中是否包含arxiv词汇
             #以下代码去除了原有网页论文名字中无法保存为window文件名的符号，采用下划线_代替
             paper_title[i] = paper_title[i].replace('"', '')
             list = paper_title[i].split(" ")
@@ -62,14 +63,16 @@ def ans(name):
             paper_title[i] = "_".join(list)
             list = paper_title[i].split("</sup>")
             paper_title[i] = "_".join(list)
-
-            print(paper_title[i])
-            path_dir = "C:\\Users\\pyl\\Desktop\\"
-            dir_list=os.listdir(path_dir)
-            path = paper_title[i] + "pdf"
-            if path not in dir_list:
-                get_pdf_arxiv(paper_web[i], path_dir + path)
-
+            s = s + paper_title[i] + '\n'
+            
+            if (paper_web[i].find("arxiv") != -1): #查看网址中是否包含arxiv词汇
+                print(paper_title[i])
+                path_dir = "C:\\Users\\pyl\\Desktop\\"
+                dir_list=os.listdir(path_dir)
+                path = paper_title[i] + "pdf"
+                if path not in dir_list:
+                    get_pdf_arxiv(paper_web[i], path_dir + path)
+    print('s='+s)
 name = input()
 ans(name)
 
