71af74c2bede3aa79f69a709971c065f9e7ea9e4
peterpango <101198369+peterpango@users.noreply.github.com>
2022-07-13 19:06:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import tkinter
 from utils import ans
 window = tkinter.Tk()
-window.title('my window')
-window.geometry('1000x700')
+window.title('window')
+window.geometry('1000x700+200+100')
  
 def plus():
-    n1 = e1.get()  # 获取输入框1的值
-    
-    t.delete(1.0, 'end')  # 清除文本框内容
-    t.insert('insert',ans(n1)) # 将结果添加到文本框显示
+    n1 = e1.get()
+    t.delete(1.0, 'end')
+    t.insert('insert',ans(n1))
  
-l1 = tkinter.Label(window, text='输入名称')
-l1.pack()
+l1 = tkinter.Label(window,
+        text='输入名称:',
+        font=('宋体',20),
+        width=10,
+        height=2).place(x=100,y=45)
 
-e1 = tkinter.Entry(window, width=15)
-e1.pack()
- 
-b1 = tkinter.Button(window, text="显示文章", command=plus)
-b1.pack()
-# 定义文本框
-t = tkinter.Text(window, state='normal', width=100, height=40)
-t.pack()
-b2 = tkinter.Button(window, text='退出', command=window.quit)
-b2.pack()
- 
+e1 = tkinter.Entry(window,
+                   width=20,
+                   bg='#d3fbfb',
+                   fg='black',
+                   font=('宋体',32),
+                   relief="groove",)
+e1.place(x=300,y=50)
+b1 = tkinter.Button(window,
+                    bg='#d3fbfb',
+                    fg='black',
+                    font=('宋体',20),
+                    width=8,
+                    relief="raised",
+                    text="显示文章",
+                    command=plus).place(x=110,y=150)
+t = tkinter.Text(window,
+                 state='normal',   
+                 bg='#d3fbfb',
+                 fg='black',
+                 font=('等线',12),
+                 width=80,
+                 height=30)
+t.place(x=300,y=150) 
 window.mainloop()
